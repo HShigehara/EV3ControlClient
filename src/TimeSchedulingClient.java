34c4e1a446f3f27db58c1c33d944f2304d291163
HShigehara <chilly_0427@hotmail.co.jp>
2016-01-05 17:28:19 +0900

    add Timer Task

--- src/TimeSchedulingClient.java
+++ src/TimeSchedulingClient.java
@@ -1,4 +1,18 @@
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
-public class TimeSchedulingClient {
+public class TimeSchedulingClient extends TimerTask{
+    private long timeStart;
+    private String id;
 
+    TimeSchedulingClient(String arg){
+	timeStart = System.nanoTime();
+	id = arg;
+    }
+
+    public void run(){
+	long timeNow = System.nanoTime() - timeStart;
+
+	System.out.println(id +  " : " + TimeUnit.MILLISECONDS.convert(timeNow, TimeUnit.NANOSECONDS));
+    }
 }
