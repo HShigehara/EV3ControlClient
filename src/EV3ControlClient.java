a96d047dea8269ae9e3981c94aca02bb244d6fce
HShigehara <chilly_0427@hotmail.co.jp>
2016-01-06 12:24:19 +0900

    delete file

--- src/EV3ControlClient.java
+++ src/EV3ControlClient.java
@@ -9,20 +9,25 @@ import java.util.Timer;
 public class EV3ControlClient{
 	//メインメソッド
     public static void main(String[] args){
-	Timer t1 = new Timer(); //タイマーt1のインスタンスを生成
+	//Timer t1 = new Timer(); //タイマーt1のインスタンスを生成
 	Timer t2 = new Timer(); //タイマーt2のインスタンスを生成
 
-	t1.schedule(new TimeSchedulingClient("First"), 1000, 100); //scheduleメソッドを利用時
-	t2.scheduleAtFixedRate(new TimeSchedulingClient("Second"), 1000, 100); //scheduleAtFixedRateメソッドを利用時
+	int flag = 0;
+	
+	//t1.schedule(new TimeSchedulingClient("First"), 1000, 100); //scheduleメソッドを利用時
+	//(~, 開始, 増分)
+	t2.scheduleAtFixedRate(new RunClient("Time Scheduling Client"), 1000, 100); //scheduleAtFixedRateメソッドを利用時
 
 	try{ //通常時
-	    Thread.sleep(5000); //5000[ms]だけ待って動作
+		while(flag == 0){
+			Thread.sleep(5000); //5000[ms]だけ待って動作を開始
+		}
 	} catch(InterruptedException e){ //例外発生時
 	    System.out.println("error : " + e); //エラーを表示
 	}
 
 	// タスクの停止
-	t1.cancel();
+	//t1.cancel();
 	t2.cancel();
     }
 }
--- src/RunClient.java
+++ src/RunClient.java
@@ -1,7 +1,31 @@
 /*
- * RunClient.java
- * クライアント(EV3)側の処理内容が記述されているクラス．
+ * TimeSchedulingClient.java
+ * クライアント(EV3)側の時間をスケジューリングするクラス．
  */
-public class RunClient {
+//インポート
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
+//クラスの定義
+public class RunClient extends TimerTask{
+	//変数
+    private long timeStart;
+    private String id;
+
+    //メソッド
+    //コンストラクタ
+    RunClient(String arg){
+    	timeStart = System.nanoTime();
+    	id = arg;
+    }
+
+    //run()．処理の流れをここに書く
+    public void run(){
+    	long timeNow = System.nanoTime() - timeStart;
+    	System.out.println(id +  " : " + TimeUnit.MILLISECONDS.convert(timeNow, TimeUnit.NANOSECONDS));
+    	
+    	//ソケット通信
+    	SocketClient socket_client = new SocketClient(); //インスタンスを生成
+    	socket_client.ConnectClient(null); //ソケット接続
+    }
 }
